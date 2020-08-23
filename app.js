149a1a5a69ce5816f05568011082e8c9d8cb6804
unknown <gohweipin98@gmail.com>
2020-08-23 14:31:02 +0800

    I am now done with the tutorial

--- app.js
+++ app.js
@@ -1,4 +1,28 @@
-const name = "WP";
-const age = "22";
+const text = document.querySelector("title");
+const addListBtn = document.querySelector(".addListBtn");
+const userList = document.querySelector(".name-list");
+const listInput = document.querySelector(".list-input");
 
-console.log(`Hello my name is ${name} and I am ${age} this year`);
+// text.classList.remove("change");
+
+// changeColor.addEventListener("click", function() {
+//   text.classList.toggle("change");
+// });
+
+// for (user of userList) {
+//   user.addEventListener("click", function() {
+//     this.classList.toggle("change");
+//   });
+// }
+
+console.log(listInput.value);
+
+addListBtn.addEventListener("click", function() {
+  //Create li
+  const newLi = document.createElement("LI");
+  const liContent = document.createTextNode(listInput.value);
+  //Add the input value into the li
+  newLi.appendChild(liContent);
+  //Attach the li to the userList
+  userList.appendChild(newLi);
+});
